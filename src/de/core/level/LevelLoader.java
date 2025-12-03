cc0a62b1ca099665c06a35142631b7d116096898
TheUnixDemon <theunixdaemon@gmail.com>
2025-12-03 22:00:34 +0100

    update

---
+++ src/de/core/level/LevelLoader.java
@@ -0,0 +1,5 @@
+package de.core.level;
+
+public class LevelLoader() {
+    
+}
--- src/de/core/level/LevelObj.java
+++ src/de/core/level/LevelObj.java
@@ -1,20 +1,30 @@
 package de.core.level;
 
+import java.io.*;
 import java.util.*;
 
+/**
+ * saves and loads level object
+ */
 public class LevelObj {
     private final String filename = "./resources/level/level.obj"; // level
     private ArrayList<Level> levels;
     public LevelObj() {
         levels = new ArrayList<>();
     }
-    public read() {
-        
+    public void addLevel(Level level) { 
+        levels.add(level);
     }
-    public write() {
-        
+    public void save() throws IOException {
+        ObjectOutputStream o = new ObjectOutputStream(new FileOutputStream(filename));
+        o.writeObject(levels); // saves levels as file
+        o.close();
     }
-    get() {
-
+    public void read() throws IOException, ClassCastException {
+        ObjectInputStream i = new ObjectInputStream(new FileInputStream(filename));
+        levels = (ArrayList<Level>) i.readObject();
+        i.close();
     }
-}
+    public ArrayList<Level> get() {
+        return levels;
+    }
